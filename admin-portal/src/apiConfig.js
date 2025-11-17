e729845ab3b03756ecd4085789a2e974ac45f01d
krishna3115 <chaitanyatandale05@gmail.com>
2025-11-17 17:35:05 +0530

    Update backend URL to HTTPS

--- admin-portal/src/apiConfig.js
+++ admin-portal/src/apiConfig.js
@@ -1,5 +1,5 @@
 // src/apiConfig.js
 //const API_BASE_URL = "http://localhost:8080"; // change as needed
-const API_BASE_URL = "http://72.61.174.80:8080";
-//const API_BASE_URL = "https://cditbl.cloud";
+//const API_BASE_URL = "http://72.61.174.80:8080";
+const API_BASE_URL = "https://cditbl.cloud:8443";
 export default API_BASE_URL;
