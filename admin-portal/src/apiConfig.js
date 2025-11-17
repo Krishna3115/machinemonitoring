42f4acf000870905b0639bab9c4b42de1c7a55d2
krishna3115 <chaitanyatandale05@gmail.com>
2025-11-17 15:36:49 +0530

    Update API URL to production

--- admin-portal/src/apiConfig.js
+++ admin-portal/src/apiConfig.js
@@ -1,4 +1,5 @@
 // src/apiConfig.js
-const API_BASE_URL = "http://localhost:8080"; // change as needed
-//const API_BASE_URL = "http://192.168.0.72:8080"
+//const API_BASE_URL = "http://localhost:8080"; // change as needed
+const API_BASE_URL = "http://72.61.174.80:8080";
+//const API_BASE_URL = "https://cditbl.cloud";
 export default API_BASE_URL;
