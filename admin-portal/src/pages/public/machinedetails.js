9c76ddbb5bac8dfaaab30c0bc7d8ca87a5f02173
krishna3115 <chaitanyatandale05@gmail.com>
2026-01-13 16:51:18 +0530

    added the notification

--- admin-portal/src/pages/public/machinedetails.js
+++ admin-portal/src/pages/public/machinedetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./Machinedetails.css";
+import "./MachineDetails.css";
 import API_BASE_URL from "../../apiConfig";
 
 export default function MachineDetails() {
